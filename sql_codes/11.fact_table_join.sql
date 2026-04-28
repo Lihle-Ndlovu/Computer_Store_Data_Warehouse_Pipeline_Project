cd2210d23a2b85e69915e820edcd669601af3aa3
Lihle Ndlovu <info.kylendlovu@gmail.com>
2026-04-28 22:18:26 +0200

    add fact table join

--- sql_codes/11.fact_table_join.sql
+++ sql_codes/11.fact_table_join.sql
@@ -1,54 +1,4 @@
-
-DROP TABLE [Staging_Computer_Store].[dbo].[Fact_Table] 
-
-CREATE TABLE [Staging_Computer_Store].[dbo].[Fact_Table](
-    [SalesID] INT IDENTITY(1,1) PRIMARY KEY,
-
-    [CustomerID] INT,
-    [ProductID] INT,
-    [StoreID] INT,
-    [LocationID] INT,
-    [DepartmentID] INT,
-    [PaymentID] INT,
-    [PriorityID] INT,
-    [ChannelID] INT,
-
-    [Cost_Price] INT,
-    [Sale_Price] INT,
-    [Discount_Amount] INT,
-    [Finance_Amount] INT,
-
-    [Credit_Score] INT,
-    [Cost_of_Repairs] INT,
-    [Total_Sales_per_Employee] INT,
-    [PC_Market_Price] INT,
-
-    [Load_date] DATETIME DEFAULT GETDATE(),
-
-    [Purchase_Date_ID] INT,
-    [Ship_Date_ID] INT
-);
-
-INSERT INTO [Staging_Computer_Store].[dbo].[Fact_Table] (
-    CustomerID,
-    ProductID,
-    StoreID,
-    LocationID,
-    DepartmentID,
-    PaymentID,
-    PriorityID,
-    ChannelID,
-    Purchase_Date_ID,
-    Ship_Date_ID,
-    Cost_Price,
-    Sale_Price,
-    Discount_Amount,
-    Finance_Amount,
-    Credit_Score,
-    Cost_of_Repairs,
-    Total_Sales_per_Employee,
-    PC_Market_Price
-)
+----Fact table join sql
 
 SELECT 
     c.CustomerID,
@@ -59,72 +9,69 @@ SELECT
     py.PaymentID,
     pr.PriorityID,
     ch.ChannelID,
+
     dp.DateID AS Purchase_Date_ID,
     ds.DateID AS Ship_Date_ID,
-    CAST(st.Cost_Price AS INT),
-    CAST(st.Sale_Price AS INT),
-    CAST(st.Discount_Amount AS INT),
-    CAST(st.Finance_Amount AS INT),
-    CAST(st.Credit_Score AS INT),
-    CAST(st.Cost_of_Repairs AS INT),
-    CAST(st.Total_Sales_per_Employee AS INT),
-    CAST(st.PC_Market_Price AS INT)
+
+    CAST(st.Cost_Price AS INT) AS Cost_Price,
+    CAST(st.Sale_Price AS INT) AS Sale_Price,
+    CAST(st.Discount_Amount AS INT) AS Discount_Amount,
+    CAST(st.Finance_Amount AS INT) AS Finance_Amount,
+    CAST(st.Credit_Score AS INT) AS Credit_Score,
+    CAST(st.Cost_of_Repairs AS INT) AS Cost_of_Repairs,
+    CAST(st.Total_Sales_per_Employee AS INT) AS Total_Sales_per_Employee,
+    CAST(st.PC_Market_Price AS INT) AS PC_Market_Price
+
 FROM [Staging_Computer_Store].[dbo].[raw_computer_sales] st
-----CUSTOMER
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Customer] AS c
+
+-- CUSTOMER
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Customer] c
     ON st.Customer_Name = c.Customer_Name
     AND st.Customer_Surname = c.Customer_Surname
     AND st.Customer_Contact_Number = c.Customer_Contact_Number
     AND st.Customer_Email_Address = c.Customer_Email_Address
 
-----PRODUCT
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Product] AS p
+-- PRODUCT
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Product] p
     ON st.PC_Make = p.PC_Make
     AND st.PC_Model = p.PC_Model
     AND st.Storage_Type = p.Storage_Type
     AND st.Storage_Capacity = p.Storage_Capacity
 
-----STORE
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Store] AS s
+-- STORE
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Store] s
     ON st.Shop_Name = s.Shop_Name
     AND st.Shop_Age = s.Shop_Age
 
-----LOCATION
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Location] AS l
+-- LOCATION
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Location] l
     ON st.Continent = l.Continent
     AND st.Country_or_State = l.Country_or_State
     AND st.Province_or_City = l.Province_or_City
 
-----DEPARTMENT
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Department] AS d
+-- DEPARTMENT
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Department] d
     ON st.Sales_Person_Name = d.Sales_Person_Name
     AND st.Sales_Person_Department = d.Sales_Person_Department
 
-----PAYMENT
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Payment] AS py
+-- PAYMENT
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Payment] py
     ON st.Payment_Method = py.Payment_Method
 
-----PRIORITY
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Priority] AS pr
+-- PRIORITY
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Priority] pr
     ON st.Priority = pr.Priority
 
-----CHANNEL
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Channel] AS ch
+-- CHANNEL
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Channel] ch
     ON st.Channel = ch.Channel
 
------DATE (ROLE-PLAYING DIMENSION)
+-- DATE (Role-playing dimension)
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Date] dp
+    ON dp.Full_Date = CAST(st.Purchase_Date AS DATE)
 
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Date] AS dp
-    ON dp.Full_Date = TRY_CONVERT(DATE, st.Purchase_Date)
-
-LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Date] AS ds
-    ON ds.Full_Date = TRY_CONVERT(DATE, st.Ship_Date_Clean)
-
+LEFT JOIN [Staging_Computer_Store].[dbo].[Dim_Date] ds
+    ON ds.Full_Date = CAST(st.Ship_Date_Clean AS DATE);
 
 
------SELECT * FROM [Staging_Computer_Store].[dbo].[Fact_Table]
-
-     
-
-
-
+---SELECT * FROM [Staging_Computer_Store].[dbo].[Fact_Table]
---
+++ sql_codes/SQLQuery6.sql
@@ -0,0 +1,37 @@
+CREATE PROCEDURE sp_fact_table
+AS
+BEGIN
+  DROP TABLE IF EXISTS [Staging_Computer_Store].[dbo].[Fact_Table];
+CREATE TABLE [Staging_Computer_Store].[dbo].[Fact_Table] (
+    SalesID INT PRIMARY KEY,
+
+    CustomerID INT,
+    ProductID INT,
+    StoreID INT,
+    LocationID INT,
+    DepartmentID INT,
+    PaymentID INT,
+    PriorityID INT,
+    ChannelID INT,
+    DateID INT,
+
+    Cost_Price DECIMAL(10,2),
+    Sale_Price DECIMAL(10,2),
+    Discount_Amount DECIMAL(10,2),
+    Finance_Amount DECIMAL(10,2),
+    Credit_Score INT,
+    Cost_of_Repairs DECIMAL(10,2),
+    Total_Sales_per_Employee DECIMAL(10,2),
+    PC_Market_Price DECIMAL(10,2),
+
+    CONSTRAINT FK_Fact_Customer FOREIGN KEY (CustomerID) REFERENCES Dim_Customer(CustomerID),
+    CONSTRAINT FK_Fact_Product FOREIGN KEY (ProductID) REFERENCES Dim_Product(ProductID),
+    CONSTRAINT FK_Fact_Store FOREIGN KEY (StoreID) REFERENCES Dim_Store(StoreID),
+    CONSTRAINT FK_Fact_Location FOREIGN KEY (LocationID) REFERENCES Dim_Location(LocationID),
+    CONSTRAINT FK_Fact_Department FOREIGN KEY (DepartmentID) REFERENCES Dim_Department(DepartmentID),
+    CONSTRAINT FK_Fact_Payment FOREIGN KEY (PaymentID) REFERENCES Dim_Payment(PaymentID),
+    CONSTRAINT FK_Fact_Priority FOREIGN KEY (PriorityID) REFERENCES Dim_Priority(PriorityID),
+    CONSTRAINT FK_Fact_Channel FOREIGN KEY (ChannelID) REFERENCES Dim_Channel(ChannelID),
+    CONSTRAINT FK_Fact_Date FOREIGN KEY (DateID) REFERENCES Dim_Date(DateID)
+)
+END;
